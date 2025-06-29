0ca4345a3185470e4dd15893ca8db208f75b35c4
toonyrafael <toonyrafael@gmail.com>
2025-06-29 00:07:11 -0300

    Adicionado Teste de For e For each, para mostrar a lista de candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -4,10 +4,25 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		selecaoCandidatos();
+		imprimirSelecionados();
 	}
 	
-	
+	static void imprimirSelecionados() {
+		String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO"};
+		
+		System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+		
+		for (int indice = 0; indice < candidatos.length; indice++) {
+			System.out.println("O candidato de numero " + (indice +1) + " é " + candidatos[indice]);
+		}
+		
+		System.out.println("Forma abreviada de interação for each");
+		
+		for (String candidato : candidatos) {
+			System.out.println("O candidato selecionado foi " + candidato);
+		}
+		
+	}
 	
 	
 	static void selecaoCandidatos() {
